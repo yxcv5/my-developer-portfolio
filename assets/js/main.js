524d464e8cdedd17a5a721ddcd4d41fa0b408376
yxcv5 <yy.wfhu@gmail.com>
2018-05-07 15:07:25 -0400

    landing page

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#landing");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+left.addEventListener("mouseenter", function(){
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", function() {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", function() {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", function() {
+  container.classList.remove("hover-right");
+});
